4a249e90f305bee7d2aa024a01f04d720a3ba6c0
Beth Miano <bethernest50@gmail.com>
2020-11-19 14:39:16 +0300

    Second Commit

--- app.py
+++ app.py
@@ -1,11 +1,14 @@
 #  Flask uses HTML/JS/CSS/Python/Jinja2
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)  # flask object takes the the name of the application
 
 # Routing
 @app.route('/home')
 def home():
-    return 'My First flask application'
+    return render_template('home.html')
+
+
+
 
 
 if __name__ == '__main__':
