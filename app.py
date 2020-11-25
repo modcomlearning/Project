8458b9d1f3b473c82d8d26950dcceb181c4ef3d5
Beth Miano <bethernest50@gmail.com>
2020-11-25 10:09:10 +0300

    Fifth Commit

--- app.py
+++ app.py
@@ -3,13 +3,14 @@ from flask import Flask, render_template
 app = Flask(__name__)  # flask object takes the the name of the application
 
 # Routing
-@app.route('/home')
+@app.route('/')
 def home():
     return render_template('home.html')
 
 
-
-
+@app.route('/login')
+def login():
+    return render_template('login.html')
 
 if __name__ == '__main__':
     app.run()
